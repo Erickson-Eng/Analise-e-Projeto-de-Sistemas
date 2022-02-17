314bf9434f49329816f0b0a57fb8b257b73d9342
Erickson Azevedo <erickson.tulio96@gmail.com>
2022-02-17 20:06:44 -0300

    first commit

---
+++ Diagrama de classe/Catalogo/backend/src/test/java/com/hotcatalogo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotcatalogo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
