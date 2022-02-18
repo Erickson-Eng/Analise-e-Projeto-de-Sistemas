4450b8797a52abe64dc92e89c9715af69fd54185
Erickson Azevedo <erickson.tulio96@gmail.com>
2022-02-17 21:51:57 -0300

    feat(MAJOR): dto creation for artist and services

---
+++ Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/application/dto/request/ArtistRequest.java
@@ -0,0 +1,16 @@
+package com.hotcatalogo.backend.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArtistRequest {
+
+    private String name;
+    private String nationality;
+}
--- Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/domain/service/IArtistService.java
+++ Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/domain/service/IArtistService.java
@@ -1,4 +1,16 @@
 package com.hotcatalogo.backend.domain.service;
 
+import com.hotcatalogo.backend.application.dto.api.ArtistApi;
+import com.hotcatalogo.backend.application.dto.request.ArtistRequest;
+import com.hotcatalogo.backend.application.dto.response.ArtistResponse;
+
 public interface IArtistService {
+
+    ArtistResponse save(ArtistRequest artistRequest);
+
+    ArtistResponse update(ArtistApi artistApi);
+
+    ArtistResponse delete(Long id);
+
+    ArtistResponse getArtist(Long id);
 }
