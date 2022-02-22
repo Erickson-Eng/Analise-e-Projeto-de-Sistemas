8011965ada3f96946f863d9e87cc584fed5acc73
Erickson Azevedo <erickson.tulio96@gmail.com>
2022-02-21 21:19:09 -0300

    feat(major): first methods created in the repositories  to search the database

--- Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/resource/repository/AlbumRepository.java
+++ Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/resource/repository/AlbumRepository.java
@@ -3,5 +3,13 @@ package com.hotcatalogo.backend.resource.repository;
 import com.hotcatalogo.backend.domain.entity.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+
+    Optional<List<Album>> findAllByArtist_Name(String artistName);
+
+    Optional<List<Album>> findAllByTitle(String title);
 }
--- Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/resource/repository/MusicRepository.java
+++ Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/resource/repository/MusicRepository.java
@@ -3,5 +3,12 @@ package com.hotcatalogo.backend.resource.repository;
 import com.hotcatalogo.backend.domain.entity.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    Optional<List<Music>> findAllByTitle(String title);
+
+    Optional<List<Music>> findAllByAlbum_ArtistName(String artist);
 }
