d89d4a08ec43379a960e80fca98b50376b4efc0a
Erickson Azevedo <erickson.tulio96@gmail.com>
2022-02-25 18:22:37 -0300

    feat: participant service created the ParticipantTableResponse dto modified return of listAllParticipantByMusic method from List<ParticipantResponse> to ParticipantTableResponse Implemented all other methods

--- Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/domain/service/mysql/ParticipantServiceMySQL.java
+++ Diagrama de classe/Catalogo/backend/src/main/java/com/hotcatalogo/backend/domain/service/mysql/ParticipantServiceMySQL.java
@@ -1,2 +1,68 @@
-package com.hotcatalogo.backend.domain.service.mysql;public class ParticipantServiceMySQL {
+package com.hotcatalogo.backend.domain.service.mysql;
+
+import com.hotcatalogo.backend.application.dto.api.ParticipantApi;
+import com.hotcatalogo.backend.application.dto.mapper.ParticipantMapper;
+import com.hotcatalogo.backend.application.dto.request.ParticipantRequest;
+import com.hotcatalogo.backend.application.dto.response.ParticipantResponse;
+import com.hotcatalogo.backend.application.dto.response.ParticipantTableResponse;
+import com.hotcatalogo.backend.domain.entity.Participant;
+import com.hotcatalogo.backend.domain.exception.ObjectNotFoundException;
+import com.hotcatalogo.backend.domain.service.IParticipantService;
+import com.hotcatalogo.backend.resource.repository.ParticipantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class ParticipantServiceMySQL implements IParticipantService {
+
+    private ParticipantRepository participantRepository;
+    private ParticipantMapper participantMapper;
+
+    @Override
+    public ParticipantResponse save(ParticipantRequest participantRequest) {
+        Participant participantEntity = participantMapper.participantRequestToEntity(participantRequest);
+        participantRepository.save(participantEntity);
+        return participantMapper.participantEntityToResponse(participantEntity);
+    }
+
+    @Override
+    public ParticipantResponse update(Long id, ParticipantApi participantApi) {
+        Participant participantEntity = verifyIfExistOrElseThrow(id);
+        BeanUtils.copyProperties(participantApi, participantEntity, "id");
+        participantRepository.save(participantEntity);
+        return participantMapper.participantEntityToResponse(participantEntity);
+    }
+
+    @Override
+    public ParticipantResponse delete(Long id) {
+        Participant participantEntity = verifyIfExistOrElseThrow(id);
+        participantRepository.delete(participantEntity);
+        return participantMapper.participantEntityToResponse(participantEntity);
+    }
+
+    @Override
+    public ParticipantTableResponse listAllParticipantByMusic(Long id) {
+        List<Participant> participantList = verifyIfExistParticipantForMusic(id);
+        List<ParticipantResponse> participantResponses = participantList
+                .stream()
+                .map(participantMapper::participantEntityToResponse)
+                .collect(Collectors.toList());
+        return new ParticipantTableResponse(participantResponses);
+    }
+
+    private Participant verifyIfExistOrElseThrow(Long id){
+        return participantRepository.findById(id)
+                .orElseThrow(ObjectNotFoundException::new);
+    }
+
+    private List<Participant> verifyIfExistParticipantForMusic(Long musicId){
+        return participantRepository.findAllByMusic_Id(musicId)
+                .orElseThrow(ObjectNotFoundException::new);
+    }
 }
